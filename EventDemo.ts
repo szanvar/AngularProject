257977be099e11bb79f1caa7fad1ccf5ed7dbdfa
szanvar <88958032+szanvar@users.noreply.github.com>
2021-08-15 10:44:57 +0530

    Addition two text box
    Take two text box and make their addition and display in template.

--- EventDemo.ts
+++ EventDemo.ts
@@ -5,7 +5,17 @@ import { Component, OnInit } from '@angular/core';
   template: `<button (click)="fun()"> Hello </button>
   {{name}}
   <label>MarvellousLabel</label>
-  <input type="text" name="" id="">`
+  <input type="text" name="" id="">
+  
+   <h3> Addition Program :</h3>
+  <label for="">Number 1 : </label>
+  <input type="text" name="newn1" #n1 />
+  <label for=""><h2> + </h2></label>
+  <label for="">Number 2 : </label>
+  <input type="text" name="newn2" #n2 /> <br><br>
+
+  <button (click)="add(n1, n2)"> ADD </button> 
+  <h3> Addition is : {{ans}} </h3>`
 })
 export class DemoComponent implements OnInit 
 {
@@ -15,7 +25,12 @@ export class DemoComponent implements OnInit
   {
     this.name = "Button Clicked";
     //console.log("Button Clicked");
-    
+  }
+  
+  ans : number | undefined;
+  add(newn1: HTMLInputElement, newn2: HTMLInputElement)
+  {
+    this.ans = parseInt(newn1.value) + parseInt(newn2.value);    
   }
   constructor() 
   {
